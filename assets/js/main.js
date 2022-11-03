524d7643a12fb38ae0c92e035152e81d57468ac4
Anton Krasnikov <krasnikovanton84@gmail.com>
2022-11-02 22:14:01 -0700

    Add current weather info

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,55 +2,64 @@
 var searchField = document.querySelector("#searchField");
 var submitBtn = document.querySelector("#submitBtn");
 var citiesOptions = document.querySelector("input");
+
+
 var coordinates ="";
+var state = "";
+
+//current date
+var today = moment();
 
 //base API URL
-var baseUrlWeather = "https://api.openweathermap.org/data/2.5/forecast?";
+var baseUrlWeatherForecast = "https://api.openweathermap.org/data/2.5/forecast?";
 var baseUrlCity = "http://api.openweathermap.org/geo/1.0/direct?q="
+var baseUrlCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?";
 
 var limitCities = 1;
+var units = "units=imperial";
 
 
 // API key
 var apiKey = "appid=81400ac056ac2215ad92e79b9c4185bb";
 
 //get coordinates by city name
-function getCoordinates(requestUrl) {
-    fetch(requestUrl)
-      .then(function (response) {
-        console.log(response.status);
-        //  Conditional for the the response.status.
-        if (response.status !== 200) {
-            console.log("not found")
-        }
-        return response.json();
-      })
-      .then(function (data) {
-        // Make sure to look at the response in the console and read how 404 response is structured.
-        coordinates = "lat="+data[0].lat+"&lon="+data[0].lon;
-      });
-  }
+async function getCoordinates(requestUrl) {
+    const responce = await fetch(requestUrl);
+    return await responce.json() 
+    .then(function(data){
+        coordinates = "lat="+data[0].lat + "&lon=" + data[0].lon;
+        state = data[0].state;
+    });
+};
 
-//get weather
-function getWeather(requestUrl) {
-    fetch(requestUrl)
-      .then(function (response) {
-        console.log(response.status);
-        //  Conditional for the the response.status.
-        if (response.status !== 200) {
-            console.log("not found")
-        }
-        return response.json();
-      })
+//get weather Current
+async function getWeatherCurrent(requestUrl) {
+    const responce = await fetch(requestUrl);
+    return await responce.json()
       .then(function (data) {
-        // Make sure to look at the response in the console and read how 404 response is structured.
+        document.querySelector("#currentCity").textContent = data.name+", "+state +"("+today.format("MM/DD/YYYY")+")";
         console.log(data);
+        document.querySelector("#currentTemp").textContent = "Temp: " + data.main.temp + " ℉";
+        document.querySelector("#currentWind").textContent = "Wind: " + data.wind.speed + " MPH";
+        document.querySelector("#currentHum").textContent = "Humidity: " + data.main.humidity + " %";
       });
 }
 
-submitBtn.addEventListener("click", function(ev){
+//get weather Forecast
+async function getWeatherForecast(requestUrl) {
+    const responce = await fetch(requestUrl);
+    return await responce.json()
+      .then(function (data) {
+        //cument.querySelector("#currentCity").textContent = data;
+      });
+}
+
+submitBtn.addEventListener("click", async function(ev){
     ev.stopPropagation();
     ev.preventDefault();
-    var requestUrl = baseUrlCity+searchField.value+"&limit="+limitCities+"&"+apiKey;
-    getCoordinates(requestUrl);
+    await getCoordinates(baseUrlCity+searchField.value+"&limit="+limitCities+"&"+apiKey);
+    await getWeatherCurrent(baseUrlCurrentWeather+coordinates+"&"+apiKey+"&"+units);
+    await getWeatherForecast(baseUrlWeatherForecast+coordinates+"&"+apiKey);
+
+
 });
